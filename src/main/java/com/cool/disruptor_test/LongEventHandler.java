0c2b13f54b28d556b310132d902f0b2eacbc8665
lengzhiyu <lengzhiyu@ushayden.com>
2018-11-27 10:32:32 +0800

    111

--- src/main/java/com/cool/disruptor_test/LongEventHandler.java
+++ src/main/java/com/cool/disruptor_test/LongEventHandler.java
@@ -7,7 +7,7 @@ public class LongEventHandler implements EventHandler<LongEvent> {
 	@Override
 	public void onEvent(LongEvent longEvent, long sequence, boolean endOfBatch) throws Exception {
 		if (longEvent.getValue() % 10000000 == 0) {
-			System.out.println(longEvent.getValue());
+			System.out.println(longEvent.getValue() + "-----" + sequence + "----" + endOfBatch);
 		}
 	}
 }
